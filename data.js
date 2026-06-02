a584313a64c0e61c9cd0fa62d68ad3cfad6f9d61
naikitthanet-hue <naikitthanet@gmail.com>
2026-06-02 15:22:44 +0700

    Update data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const gameData = {
+    adventure: [
+        { q: "[Verb to be] I ___ a student.", left: "am", right: "is", ans: "left" },
+        { q: "[Past Simple] He ___ to school yesterday.", left: "go", right: "went", ans: "right" },
+        { q: "[Future Simple] We ___ play games tomorrow.", left: "will", right: "played", ans: "left" },
+        { q: "[Verb to be] They ___ very happy.", left: "are", right: "am", ans: "left" },
+        { q: "[Past Simple] I ___ pizza last night.", left: "eat", right: "ate", ans: "right" }
+    ],
+    vocabulary: [
+        { q: "[My Body] หัว", left: "Head", right: "Hand", ans: "left" },
+        { q: "[Feelings] มีความสุข", left: "Sad", right: "Happy", ans: "right" },
+        { q: "[Weather] ฝนตก", left: "Rainy", right: "Sunny", ans: "left" },
+        { q: "[Daily Routine] ตื่นนอน", left: "Wake up", right: "Sleep", ans: "left" },
+        { q: "[Sports & Hobbies] ฟุตบอล", left: "Tennis", right: "Football", ans: "right" },
+        { q: "[School & Subjects] วิชาคณิตศาสตร์", left: "Math", right: "Art", ans: "left" }
+    ]
+};
